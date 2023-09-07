2f1ecee742b4477c150fa88ab652414d6676cf89
KalpaDS <desilvakalpa5@gmail.com>
2023-09-07 13:47:45 +0530

    #Simple layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
+import {FcTodoList} from "react-icons/fc";
 
 const style = {
-    bg: "h-screen w-screen"
+    bg: "h-screen w-screen p-4 bg-gradient-to-r from-[#d6cfc7] to-[#222021]"
 }
 
 function App() {
     return (
-        <div>
-            <h1 className={style.bg}>
-                Hello world!
-            </h1>
+        <div className={style.bg}>
+            <div className={style.container}>
+                <h3 className={style.heading}>TODO APP</h3>
+                <form className={style.form}>
+                    <input className={style.input} type="text" placeholder='add todo'/>
+                    <button className={style.button}><FcTodoList size={30}/></button>
+                </form>
+                <ul>
+
+                </ul>
+            </div>
         </div>
     );
 }
