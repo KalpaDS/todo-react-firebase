e2e3d864d898aaf520a919f1d4c05297e308a08e
KalpaDS <desilvakalpa5@gmail.com>
2023-09-07 16:00:01 +0530

    #Create todos in firebase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 import {FcTodoList} from "react-icons/fc";
 import Todo from "./Todo";
 import {db} from "./firebase";
-import {collection, query, doc, onSnapshot, updateDoc} from 'firebase/firestore';
+import {collection, query, doc, onSnapshot, updateDoc, addDoc} from 'firebase/firestore';
 
 const style = {
     bg: "h-screen w-screen p-4 bg-gradient-to-r from-[#d6cfc7] to-[#222021]",
@@ -16,7 +16,22 @@ const style = {
 
 function App() {
     const [todos, setTodos] = useState([]);
+    const [input, setInput] = useState('');
+    //console.log(input);
 
+    //Create todos
+    const createTodo = async (r) => {
+        r.preventDefault(r);
+        if (input === '') {
+            alert("Please enter a Todo..!!");
+            return
+        }
+        await addDoc(collection(db, 'todos'), {
+            text: input,
+            completed: false
+        })
+        setInput('')
+    };
 
     //Read_todo_from_firebase
     useEffect(() => {
@@ -42,13 +57,15 @@ function App() {
         <div className={style.bg}>
             <div className={style.container}>
                 <h3 className={style.heading}>TODO APP</h3>
-                <form className={style.form}>
-                    <input className={style.input} type="text" placeholder='add todo'/>
+                <form onSubmit={createTodo} className={style.form}>
+                    <input value={input}
+                           onChange={(r) => setInput(r.target.value)}
+                           className={style.input} type="text" placeholder='add todo'/>
                     <button className={style.button}><FcTodoList size={30}/></button>
                 </form>
                 <ul>
                     {todos.map((todo, index) => (
-                        <Todo key={index} todo={todo} toggleComplete={toggleComplete}/>
+                            <Todo key={index} todo={todo} toggleComplete={toggleComplete}/>
                         )
                     )}
                 </ul>
