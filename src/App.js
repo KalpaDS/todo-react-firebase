46faf733c3f6bea12a25579709f789eb6d252e60
KalpaDS <desilvakalpa5@gmail.com>
2023-09-07 14:19:36 +0530

    #main styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Todo from "./Todo";
 const style = {
     bg: "h-screen w-screen p-4 bg-gradient-to-r from-[#d6cfc7] to-[#222021]",
     container: 'bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4',
-    heading: 'text-3xl font-bold text-center text-grey-800 p-2'
+    heading: 'text-3xl font-bold text-center text-grey-800 p-2',
+    form: 'flex justify-between',
+    input: 'border p-2 w-full text-xl',
+    button: 'border p-4 ml-2 text-slate-100',
+    count: 'text-center p-2'
 }
 
 function App() {
